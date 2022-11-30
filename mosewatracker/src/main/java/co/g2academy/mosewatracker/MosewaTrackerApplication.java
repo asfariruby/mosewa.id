e11a890ccdbb9decc09d135c95a42d71145b5a11
asfariruby <ammar.asfari@gmail.com>
2022-12-01 00:40:19 +0700

    Adding MosewaTracker
    Add MosewaTracker as microservice

---
+++ mosewatracker/src/main/java/co/g2academy/mosewatracker/MosewaTrackerApplication.java
@@ -0,0 +1,13 @@
+package co.g2academy.mosewatracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MosewaTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MosewaTrackerApplication.class, args);
+	}
+
+}
